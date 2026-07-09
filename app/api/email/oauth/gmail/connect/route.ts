ee3e731ad736b08cae53cdf4d6cee63fb46cb62d
Dhiren Kirpalani <dhirenkirpalani@Dhirens-MacBook-Pro.local>
2026-07-09 11:06:33 +0700

    chore: update integration

--- app/api/email/oauth/gmail/connect/route.ts
+++ app/api/email/oauth/gmail/connect/route.ts
@@ -21,10 +21,10 @@ export async function GET(req: NextRequest) {
     const scopes = [
       "openid",
       "https://www.googleapis.com/auth/userinfo.email",
-      "https://www.googleapis.com/auth/gmail.send",
       "https://www.googleapis.com/auth/gmail.readonly",
       "https://www.googleapis.com/auth/gmail.modify",
       "https://www.googleapis.com/auth/gmail.metadata",
+      "https://www.googleapis.com/auth/gmail.labels",
     ].join(" ")
 
     const url = new URL("https://accounts.google.com/o/oauth2/v2/auth")
